eb26a9731d04e0b0e37eb5b009ee092af9cb9d2f
key5771 <ksj57715@hanmail.net>
2020-06-30 14:07:16 +0900

    트렌딩, 이슈, 엔터 버튼 클릭시 class='active' 변경, fetch url 변경

--- src/index.js
+++ src/index.js
@@ -2,10 +2,39 @@ import { fetchUrl } from './fetch-url';
 
 const { default: dataModel } = require('./data-model');
 
-const $list = document.getElementById('list');
+const $trending = document.getElementById('trending');
+const $issue = document.getElementById('issue');
+const $enter = document.getElementById('enter');
 
 window.addEventListener('load', (event) => {
   fetchUrl('https://1boon.kakao.com/ch/trending.json').then((data) => {
     console.log(data.data);
   });
+});
+
+$trending.addEventListener('click', function () {
+  fetchUrl('https://1boon.kakao.com/ch/trending.json').then((response) =>
+    console.log(response.data),
+  );
+  $trending.className = 'active';
+  $issue.className = '';
+  $enter.className = '';
+});
+
+$issue.addEventListener('click', function () {
+  fetchUrl('https://1boon.kakao.com/ch/issue.json').then((response) =>
+    console.log(response.data),
+  );
+  $trending.className = '';
+  $issue.className = 'active';
+  $enter.className = '';
+});
+
+$enter.addEventListener('click', function () {
+  fetchUrl('https://1boon.kakao.com/ch/enter.json').then((response) =>
+    console.log(response.data),
+  );
+  $trending.className = '';
+  $issue.className = '';
+  $enter.className = 'active';
 });
